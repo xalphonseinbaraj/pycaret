9d0e9020be4f63638a75a456516b77d3a8ce6504
xalphonseinbaraj <47241538+xalphonseinbaraj@users.noreply.github.com>
2020-11-24 12:45:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -31,5 +31,4 @@ def predict_api():
     return jsonify(output)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',port=5000)
-    #app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
+    app.run(host='127.0.0.1',port=8000)  
